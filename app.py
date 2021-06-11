5bd7dc66eccf511a4b24c7428035a366587c9001
Monica <tamang.monica@gmail.com>
2021-06-11 16:33:18 -0600

    Created a function that checks the validity of the animal name, added comments to code

--- app.py
+++ app.py
@@ -2,212 +2,307 @@ import dbconnect
 import mariadb
 from flask import Flask, request, Response
 import json
+import re
 import traceback
 
-def close_db_and_cursor(conn, cursor):
+# Creating a function that searches for invalid characters in the animal's name
+def check_invalid_chars(user_input):
+    # Using the python's regular expression compile and search methods to find whether the user's data contains invalid characters for the animal name
+    valid_chars = re.compile(r'[a-zA-Z]').search(user_input)
+    # If the animal name contains only lowercase and uppercase letters, store it in the database
+    if(valid_chars):
+        return True
+    # if the animal name is contains invalid characters such as numbers or special characters, don't store it in the database
+    else:
+        return False
+
+# Creating a function that closes the database connection and the cursor
+def close_db_connection_and_cursor(conn, cursor):
+    # Closing the cursor and database connection
     closing_cursor = dbconnect.close_cursor(cursor)
     closing_db = dbconnect.close_db_connection(conn)
+    # If the cursor or database connection failed to close, print an error message
     if(closing_cursor == False or closing_db == False):
-        print("\nFailed to close cursor and database connection.")
+        print("Failed to close cursor and database connection.")
 
+# Checking to see if the database connection is opened and the cursor is created
 def check_db_connection_and_cursor(conn, cursor):
+    # If the connection was successful but the cursor was failed to be created, print an error message and attempt to close the cursor and database connection again
     if(conn == None or cursor == None):
         print("An error has occured in the database.")
         dbconnect.close_cursor(cursor)
         dbconnect.close_db_connection(conn)
         return
 
+# Initializing the flask server
 app = Flask(__name__)
 
+# Creating a GET request to the "animals" endpoint to get the list of animals
 @app.get("/animals")
 def get_animals_list():
+    # Opening the database and creating a cursor
     conn = dbconnect.open_db_connection()
     cursor = dbconnect.create_db_cursor(conn)
+
+    # Initalizing the list of animals as a variable and assigning it a value of "None" so that it can still be referenced after the try-except block
     animals_list = None
 
+    # Checking to see if the database connection is opened and whether the cursor is created
     check_db_connection_and_cursor(conn, cursor)
 
+    # Creating a try-except block to catch errors when getting the list of animals from the database
     try:
-        cursor.execute("SELECT name, class, location, id FROM animal")
+        # Getting the all the animals from the database
+        cursor.execute("SELECT name, id FROM animal")
         animals_list = cursor.fetchall()
+    # Raising the OperationalError exception for things that are not in control of the programmer, printing an error message and the traceback
     except mariadb.OperationalError:
-        print(f"\nOperational errors occured when retrieving the animals list from the database.\n")
+        print(f"An operational error has occured when retrieving the all the animals from the database.")
         traceback.print_exc()
+    # Raising the ProgrammingError exception for errors made by the programmer, printing an error message and the traceback
     except mariadb.ProgrammingError:
-        print("\nInvalid SQL syntax.\n")
+        print("Invalid SQL syntax.")
         traceback.print_exc()
+    # Raising the DatabaseError exception for errors related to the database, printing an error message and the traceback
     except mariadb.DatabaseError:
-        print("\nError detected in the database and resulted in a connection failure.\n")
+        print("Error detected in the database and resulted in a connection failure.")
         traceback.print_exc()
+    # Raising a general exception to catch all other errors, printing a general error message and the traceback
     except:
         print("An error has occured.")
         traceback.print_exc()
 
-    close_db_and_cursor(conn, cursor)
+    # Closing the cursor and database connection
+    close_db_connection_and_cursor(conn, cursor)
 
+    # If the list of animals was successfully retrieved from the database, convert the list of animals into JSON format and send a client success response
     if(animals_list != None):
         animals_list_json = json.dumps(animals_list, default=str)
         return Response(animals_list_json, mimetype="application/json", status=200)
+    # If the list of animals was not retrieved from the database, send the user a server error response
     else:
         return Response("Failed to retrieve animals from database.", mimetype="text/plain", status=500)
 
+# Creating a POST request to the "animals" endpoint to create an animal
 @app.post("/animals")
 def create_animal():
+    # Creating a try-except block to catch errors when receiving the user's input
     try:
-        animal_name = request.json['name']
-        animal_class = request.json['class']
-        animal_location = request.json['location']
-    except TypeError:
-        print("\nInvalid data being passed to the database.\n")
+        # Converting the user's data as a string
+        animal_name = str(request.json['name'])
+        # Checking the validity of the animal name
+        is_animal_name_valid = check_invalid_chars(animal_name)
+        # If the user sent an invalid animal name, send the user a client error response
+        if(is_animal_name_valid == False):
+            return Response("Invalid animal name being passed to the database.", mimetype="text/plain", status=400) 
+    # Raising the error exception if the regular expression library is unable to compile the data being passed to it
+    except re.error:
+        print("An error occured with processing the regular expression.")
         traceback.print_exc()
+    # Raising the ValueError exception if the user sends an animal name that has a data type other than a string, printing an error message and the traceback
     except ValueError:
-        print("\nInvalid data being passed to the database.\n")
+        print("Invalid animal name being passed to the database.")
         traceback.print_exc()
+    # Raising a general exception to catch all other errors, printing a general error message and the traceback
     except:
         print("An error has occured.")
         traceback.print_exc()
-        return Response("Invalid data was passed to the database.", mimetype="text/plain", status=400)
+        # Sending the user a client error response and stopping the function from running the next lines of code that interacts with the database
+        return Response("Invalid animal name was passed to the database.", mimetype="text/plain", status=400)
 
+    # If the user sends valid data, open the database connection and create a cursor
     conn = dbconnect.open_db_connection()
     cursor = dbconnect.create_db_cursor(conn)
+
+    # Initializing the row count and id and assigning it a value so that it can still be referenced after the try-except block
     row_count = 0
     new_id = None
 
+    # Checking to see if the database connection is opened and whether the cursor is created
     check_db_connection_and_cursor(conn, cursor)
 
+    # Creating a try-except block to catch errors when inserting the user's data into the database
     try:
-        cursor.execute("INSERT INTO animal(name, class, location) VALUES(?, ?, ?)", [animal_name, animal_class, animal_location])
+        # Inserting the user's data into the database and commiting the changes
+        cursor.execute("INSERT INTO animal(name) VALUES(?)", [animal_name,])
         conn.commit()
+        # Checking to see if the user's data was stored in the database and getting the id of new animal
         row_count = cursor.rowcount
         new_id = cursor.lastrowid
+    # Raising an IntegrityError exception if the user sends an animal that already exists in the database, printing an error message and the traceback
     except mariadb.IntegrityError:
-        print(f"\nAnimal name is already taken. Please enter another username.\n")
+        print(f"Unique key constraint failure. The animal already exists in the database.")
         traceback.print_exc()
+    # Raising the OperationalError exception for things that are not in control of the programmer, printing an error message and the traceback
     except mariadb.OperationalError:
-        print(f"\nAn operational error has occured when creating {animal_name} and storing {animal_name} in the database.\n")
+        print(f"An operational error has occured when creating a new animal.")
         traceback.print_exc()
+    # Raising the ProgrammingError exception for errors made by the programmer, printing an error message and the traceback
     except mariadb.ProgrammingError:
-        print(f"\nInvalid SQL syntax.\n")
+        print(f"Invalid SQL syntax.")
         traceback.print_exc()
+    # Raising the DatabaseError exception for errors related to the database, printing an error message and the traceback
     except mariadb.DatabaseError:
-        print(f"\nAn error in the database has occured. Failed to create {animal_name} and store {animal_name} in the database.\n")
+        print(f"An error in the database has occured. Failed to create a new animal.")
         traceback.print_exc()
+    # Raising a general exception to catch all other errors, printing a general error message and the traceback
     except:
         print("An error has occured.")
         traceback.print_exc()
 
-    close_db_and_cursor(conn, cursor)
+    # Closing the cursor and database connection
+    close_db_connection_and_cursor(conn, cursor)
 
+    # If the user's data was stored in the database and an id was created for the new animal, send the user the new animal created in JSON format and a client success response
     if(row_count == 1 and new_id != None):
         new_animal = {
             'id': new_id,
-            'name': animal_name,
-            'class': animal_class,
-            'location': animal_location
+            'name': animal_name
         }
         new_animal_json = json.dumps(new_animal, default=str)
         return Response(new_animal_json, mimetype="application/json", status=201)
+    # If the database failed to store the user's animal, send the user a server error response
     else:
         return Response("Failed to create a new animal.", mimetype="text/plain", status=500)
 
-
+# Creating a PATCH request to the "animals" endpoint to edit an animal
 @app.patch("/animals")
 def edit_animal():
+    # Creating a try-except block to catch errors when receiving the user's data
     try:
-        animal_id = request.json['id']
-        animal_name = request.json['name']
-    except TypeError:
-        print("\nInvalid data being passed to the database.\n")
+        # Converting the id into an integer data type and converting the name into a string data type
+        animal_id = int(request.json['id'])
+        animal_name = str(request.json['name'])
+        # Checking the validity of the animal name
+        is_animal_name_valid = check_invalid_chars(animal_name)
+        # If the user sent an invalid animal name, send the user a client error response
+        if(is_animal_name_valid == False):
+            return Response("Invalid animal name being passed to the database.", mimetype="text/plain", status=400)
+    # Raising the error exception if the regular expression library is unable to compile the data being passed to it
+    except re.error:
+        print("An error occured with processing the regular expression.")
         traceback.print_exc()
+    # Raising the ValueError exception if the user sends an invalid data type for the id and name, printing an error message and the traceback
     except ValueError:
-        print("\nInvalid data being passed to the database.\n")
+        print("Invalid data was being passed to the database.")
         traceback.print_exc()
+    # Raising a general exception to catch all other errors, printing a general error message and the traceback
     except:
         print("An error has occured.")
         traceback.print_exc()
-        return Response("Invalid data was passed to the database.", mimetype="text/plain", status=400)
+        # Sending the user a client error response and stopping the function for running the next lines of code that interacts with the database
+        return Response("Invalid data was being passed to the database.", mimetype="text/plain", status=400)
 
+    # If the user sends valid data, open the database connection and create a cursor 
     conn = dbconnect.open_db_connection()
     cursor = dbconnect.create_db_cursor(conn)
-    row_count = 0
-    edit_animal = None
 
+    # Initializing the row count and assigning it a value so that it can still be referenced after the try-except block
+    row_count = 0
+
+    # Checking to see if the database connection is opened and whether the cursor is created
     check_db_connection_and_cursor(conn, cursor)
 
+    # Creating a try-except block to catch errors when updating the data in the database
     try:
+        # Editing the old animal with the new animal and committing the changes
         cursor.execute("UPDATE animal SET name = ? WHERE id = ?", [animal_name, animal_id])
         conn.commit()
+        # Checking to see if the user's data was stored in the database
         row_count = cursor.rowcount
-        
-        cursor.execute("SELECT name, class, location, id FROM animal WHERE id = ?", [animal_id,])
-        edit_animal = cursor.fetchall()
-    except mariadb.IntegrityError:
-        print(f"\nAnimal name is already taken. Please enter another username.\n")
-        traceback.print_exc()
+    # Raising the OperationalError exception for things that are not in control of the programmer, printing an error message and the traceback
     except mariadb.OperationalError:
-        print(f"\nAn operational error has occured when creating {animal_name} and storing {animal_name} in the database.\n")
+        print(f"An operational error has occured when storing the edited animal in the database.")
         traceback.print_exc()
+    # Raising the ProgrammingError exception for errors made by the programmer, printing an error message and the traceback
     except mariadb.ProgrammingError:
-        print(f"\nInvalid SQL syntax.\n")
+        print(f"Invalid SQL syntax.")
         traceback.print_exc()
+    # Raising the DatabaseError exception for errors related to the database, printing an error message and the traceback
     except mariadb.DatabaseError:
-        print(f"\nAn error in the database has occured. Failed to create {animal_name} and store {animal_name} in the database.\n")
+        print(f"An error in the database has occured. Failed to stored edited animal in the database.")
         traceback.print_exc()
+    # Raising a general exception to catch all other errors, printing a general error message and the traceback
     except:
         print("An error has occured.")
         traceback.print_exc()
 
-    close_db_and_cursor(conn, cursor)
+    # Closing the cursor and database connection
+    close_db_connection_and_cursor(conn, cursor)
 
-    if(row_count == 1 and edit_animal != None):
+    # If the edited animal was successfully stored into the database, send the user the edited animal in JSON format and a client success response
+    if(row_count == 1):
+        edit_animal = {
+            'id': animal_id,
+            'name': animal_name
+        }
         edit_animal_json = json.dumps(edit_animal, default=str)
-        return Response(edit_animal_json, mimetype="application/json", status=201)
+        return Response(edit_animal_json, mimetype="application/json", status=200)
+    # If the database failed to store the edited animal, send the user a server error response
     else:
         return Response("Failed to edit animal.", mimetype="text/plain", status=500)
 
+# Creating a DELETE request to the "animals" endpoint to delete an exisiting animal
 @app.delete("/animals")
 def delete_animal():
+    # Creating a try-except block to catch error when receiving data from the user
     try:
-        animal_id = request.json['id']
-    except TypeError:
-        print("\nInvalid data being passed to the database.\n")
-        traceback.print_exc()
+        # Converting the id into an integer data type
+        animal_id = int(request.json['id'])
+    # Raising the ValueError exception if the user sends an id that has a data type other than an integer, printing an error message and the traceback
     except ValueError:
         print("\nInvalid data being passed to the database.\n")
         traceback.print_exc()
+    # Raising a general exception to catch all other errors, printing a general error message and the traceback
     except:
         print("An error has occured.")
         traceback.print_exc()
+        # Sending the user a client error response and stopping the function for running the next lines of code that interacts with the database
         return Response("Invalid data was passed to the database.", mimetype="text/plain", status=400)
 
+    # If the user sends a valid id, open the database connection and create a cursor
     conn = dbconnect.open_db_connection()
     cursor = dbconnect.create_db_cursor(conn)
+
+    # Initializing the row count and assigning it a value so that it can still be referenced after the try-except block
     row_count = 0
 
+    # Checking to see if the database connection is opened and whether the cursor is created
     check_db_connection_and_cursor(conn, cursor)
 
+    # Creating a try-except block to catch errors when deleting an animal from the database
     try:
+        # Deleting an animal from the database and committing the changes
         cursor.execute("DELETE FROM animal WHERE id = ?", [animal_id,])
         conn.commit()
+        # Checking to see if the user's data was stored in the database
         row_count = cursor.rowcount
+    # Raising the OperationalError exception for things that are not in control of the programmer, printing an error message and the traceback
     except mariadb.OperationalError:
         print(f"\nAn operational error has occured. Failed to delete animal in the database.\n")
         traceback.print_exc()
+    # Raising the ProgrammingError exception for errors made by the programmer, printing an error message and the traceback
     except mariadb.ProgrammingError:
         print(f"\nInvalid SQL syntax.\n")
         traceback.print_exc()
+    # Raising the DatabaseError exception for errors related to the database, printing an error message and the traceback
     except mariadb.DatabaseError:
         print(f"\nAn error in the database has occured. Failed to delete animal in the database.\n")
         traceback.print_exc()
+    # Raising a general exception to catch all other errors, printing a general error message and the traceback
     except:
         print("An error has occured.")
         traceback.print_exc()
 
-    close_db_and_cursor(conn, cursor)
+    # Closing the cursor and database connection
+    close_db_connection_and_cursor(conn, cursor)
 
+    # If the database successfully deleted the animal, send a client success response
     if(row_count == 1):
-        return Response(f"Animal {animal_id} has been successfully deleted.", mimetype="application/json", status=200)
+        return Response(f"Animal {animal_id} was successfully deleted.", mimetype="application/json", status=200)
+    # If the database failed to delete the animal, send a server error response
     else:
         return Response("Failed to delete animal.", mimetype="text/plain", status=500)
 
+# Running the flask server with debug mode turned on
 app.run(debug=True)
--- dbconnect.py
+++ dbconnect.py
@@ -2,69 +2,97 @@ import dbcreds
 import mariadb
 import traceback
 
+# Creating a function that opens that database connection
 def open_db_connection():
+    # Using a try-except block to catch errors when connecting to the database
     try:
+        # Trying to return the connection object to the caller
         return mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+    # Raising the OperationalError exception for things that are not in control of the programmer, printing an error message and the traceback
     except mariadb.OperationalError:
-        print("\nOperational errors detected in the database connection.\n")
+        print("Operational errors detected in the database connection.")
         traceback.print_exc()
+    # Raising the DatabaseError exception for errors related to the database, printing an error message and the traceback
     except mariadb.DatabaseError:
-        print("\nError detected in the database and resulted in a connection failure.\n")
+        # A DatabaseError exception is raised for all errors that are related to the database
+        print("Error detected in the database and resulted in a connection failure.")
         traceback.print_exc()
+    # Raising a general exception to catch all other errors, printing a general error message and the traceback
     except:
-        print("\nAn error has occured. Failed to connect to the database.\n")
+        print("An error has occured. Failed to connect to the database.")
         traceback.print_exc()
-        return None
 
+# Creating a function that returns a cursor object using the current connection, which is passed as an argument
 def create_db_cursor(conn):
+    # Using a try-except block to catch errors when creating a cursor
     try:
+        # Trying to return a cursor object using the current connection
         return conn.cursor()
+    # Raising an InternalError exception if the cursor is invalid
     except mariadb.InternalError:
-        print("\nInternal errors detected in the database. Failed to create a cursor.\n")
+        print("Internal errors detected in the database. Failed to create a cursor.")
         traceback.print_exc()
+    # Raising the OperationalError exception for things that are not in control of the programmer, printing an error message and the traceback
     except mariadb.OperationalError:
-        print("\nOperational errors detected in the database connection. Failed to create a cursor.\n")
+        print("Operational errors detected in the database connection. Failed to create a cursor.")
         traceback.print_exc()
+    # Raising the DatabaseError exception for errors related to the database, printing an error message and the traceback
     except mariadb.DatabaseError:
-        print("\nErrors detected in database. Failed to create a cursor.\n")
+        print("Errors detected in database. Failed to create a cursor.")
         traceback.print_exc()
+    # Raising a general exception to catch all other errors, printing a general error message and the traceback
     except:
-        print("\nAn error has occured. Failed to create a cursor.\n")
+        print("An error has occured. Failed to create a cursor.")
         traceback.print_exc()
-        return None
 
+# Creating a function that closes the cursor
 def close_cursor(cursor):
+    # Checking to see if the cursor was initially created and if it wasn't, don't attempt to close the cursor
     if(cursor == None):
         return True
+    # Using a try-except block to catch errors when closing the cursor
     try:
+        # Trying to close the cursor, returning "True" to indicate that the cursor was closed successfully
         cursor.close()
         return True
+    # Raising an InternalError exception if the cursor is invalid
     except mariadb.InternalError:
-        print("\nInternal errors detected in the database. Failed to create a cursor.\n")
+        print("Internal errors detected in the database. Failed to create a cursor.")
         traceback.print_exc()
+    # Raising the OperationalError exception for things that are not in control of the programmer, printing an error message and the traceback
     except mariadb.OperationalError:
-        print("\nOperational errors detected in the database connection. Failed to create a cursor.\n")
+        print("Operational errors detected in the database connection. Failed to create a cursor.")
         traceback.print_exc()
+    # Raising the DatabaseError exception for errors related to the database, printing an error message and the traceback
     except mariadb.DatabaseError:
-        print("\nErrors detected with the current connection. Failed to close cursor.\n")
+        # A DatabaseError exception is raised for all errors that are related to the database
+        print("Errors detected with the current connection. Failed to close cursor.")
         traceback.print_exc()
+    # Raising a general exception to catch all other errors, printing a general error message and the traceback
     except:
         print("An error has occured. Failed to close cursor.")
         traceback.print_exc()
         return False
 
+# Creating a function that closes the database connection
 def close_db_connection(conn):
+    # If the database connect was not initially opened, don't attempt to close it
     if(conn == None):
         return True
+    # Using a try-except block to catch errors when closing the database connection
     try:
+        # Trying to close the connection, returning "True" to indicate that the connection was close successfully
         conn.close()
         return True
+    # Raising the OperationalError exception for things that are not in control of the programmer, printing an error message and the traceback
     except mariadb.OperationalError:
-        print("\nOptional errors detected in the database. Failed to close the connection.\n")
+        print("Optional errors detected in the database. Failed to close the connection.")
         traceback.print_exc()
+    # Raising the DatabaseError exception for errors related to the database, printing an error message and the traceback
     except mariadb.DatabaseError:
         print("Errored detected in the database. Failed to close the connection.")
         traceback.print_exc()
+    # Raising a general exception to catch all other errors, printing a general error message and the traceback
     except:
         print("An error has occured. Failed to close database connection.")
         traceback.print_exc()
